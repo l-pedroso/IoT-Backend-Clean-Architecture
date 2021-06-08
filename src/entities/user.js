e3159e2885f381bb7544d50b0d86f564944a3c7a
lpedroso-mg <65230855+lpedroso-mg@users.noreply.github.com>
2021-06-08 07:25:54 -0300

    some changes

--- src/entities/user.js
+++ src/entities/user.js
@@ -1,10 +1,9 @@
 module.exports = class User{ 
 
-    constructor(firstName, lastName, email, devices) {
+    constructor({firstName, lastName, email, devices}) {
         this.id = null;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.fullName = `${firstName} ${lastName}`;
         this.email = email;
         this.devices = devices;
     }
--- src/use_cases/add_user.js
+++ src/use_cases/add_user.js
@@ -1,29 +1,19 @@
 
-const newUser = async (UserInfo, UserRepository) => {
-    
+const addUser = async (UserInfo, UserRepository) => {
+
     if(UserInfo.email_verified === false){
         throw new Error('email not verified');
     }
-    
     try{
         const info = await UserRepository.findByEmail(UserInfo.email);
-
         if(info != null){
-    
             throw new Error('user aready in database');
         }
-    
         await UserRepository.add(UserInfo);
-
     }
     catch(e){
         throw(e);
     }
-
-
-    
-    
-    
 }
 
-exports.addUser = newUser;
+exports.add = newUser;
